28a66bf8e1c4da18557bc1199381c7ccfb1a690e
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-10-18 19:43:26 +0200

    14

---
+++ A-Beginner/14/script.js
@@ -0,0 +1,14 @@
+//Skriv en funktion som accepterar tre argument
+
+function func1(a, b, c) {
+    console.log(arguments[0]);
+    // expected output: 1
+  
+    console.log(arguments[1]);
+    // expected output: 2
+  
+    console.log(arguments[2]);
+    // expected output: 3
+  }
+  
+  func1(1, 2, 3);
